c97196d3f3d1ac6ba0cf8193ce2a296985c96f4d
Tony.Liu <tony.liu@inctw.com.tw>
2021-11-22 18:03:54 +0800

    新增讓球Destroy的條件。

--- Assets/Scrips/PingPongBallLuncher.cs
+++ Assets/Scrips/PingPongBallLuncher.cs
@@ -6,6 +6,7 @@ public class PingPongBallLuncher : MonoBehaviour
 {
     public float lunchForce = 5.0f;
     private Rigidbody m_rigidbody;
+    private bool addForceStatus = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,25 @@ public class PingPongBallLuncher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        m_rigidbody.AddForce(Vector3.forward * lunchForce * -1);
+        if (true == addForceStatus)
+            m_rigidbody.AddForce(Vector3.forward * lunchForce * -1);
+
+        /* 球太低、發球台射出後速度趨近靜止，即刪除球物件 */
+        if (addForceStatus == false)
+        {
+            if ((transform.position.y < 0.1)
+            || (m_rigidbody.velocity.magnitude < 0.1))
+                Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        /* 碰觸到球桌，球停止加速 */
+        if(other.gameObject.CompareTag("PingPongTable"))
+        {
+            Debug.Log("PingPongTable trigger!");
+            addForceStatus = false;
+        }
     }
 }
