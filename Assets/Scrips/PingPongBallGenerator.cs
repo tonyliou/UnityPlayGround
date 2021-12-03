3e0efae64bb42e9194c3a04eb19c485612f05c1f
Tony.Liu <tony.liu@inctw.com.tw>
2021-12-03 11:45:45 +0800

    修改發球機的球偏移問題

--- Assets/Scrips/PingPongBallGenerator.cs
+++ Assets/Scrips/PingPongBallGenerator.cs
@@ -24,7 +24,7 @@ public class PingPongBallGenerator : MonoBehaviour
             this.delta = 0;
             GameObject go = Instantiate(pingPongBallPrefab) as GameObject;
 
-            float px = Random.Range(-1.0f, 1.0f);
+            float px = Random.Range(-2.2f, 0.3f);
             ballLuncherGun.transform.localPosition = new Vector3(px, 3.6f, 0.6f);
             Vector3 ballPosition = ballLuncherGun.transform.position;
             go.transform.position = ballPosition;
